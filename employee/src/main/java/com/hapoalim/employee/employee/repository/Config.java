47fec78149f6ead6b379020e850f9a539b738e00
Benaya <benaya7@gmail.com>
2021-07-05 19:55:55 +0300

    added use of env var for choosing host

--- employee/src/main/java/com/hapoalim/employee/employee/repository/Config.java
+++ employee/src/main/java/com/hapoalim/employee/employee/repository/Config.java
@@ -1,39 +1,41 @@
 package com.hapoalim.employee.employee.repository;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+// import java.net.InetAddress;
+import java.net.URI;
+// import java.net.UnknownHostException;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+// import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+// import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+// import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.baeldung.spring.data.es.repository")
-@ComponentScan(basePackages = { "com.baeldung.spring.data.es.service" })
 public class Config {
+    @Autowired
+    private Environment env;
 
     @Bean
     public RestHighLevelClient client() {
-        try {
-            System.out.println(InetAddress.getLocalHost());
-            System.out.println(InetAddress.getByName("elasticsearch"));
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("elasticsearch:9200")
-                .build();
+        final String stringUrl = env.getProperty("ELASTICSEARCH_URL");
+        System.out.println(stringUrl);
+        final URI uri = URI.create(stringUrl);
+
+        String host = uri.getHost();
+        int port = 9200;
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo(host + ":" + port).build();
 
         return RestClients.create(clientConfiguration).rest();
     }
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
-    }
+    // @Bean
+    // public ElasticsearchOperations elasticsearchTemplate() {
+    // return new ElasticsearchRestTemplate(client());
+    // }
 }
